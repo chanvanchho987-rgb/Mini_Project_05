cd7e2b6e96718b943e886002a53d2e5b8e4fa68e
sva <chanvanchho987@gmail.com>
2026-06-22 16:27:33 +0700

    u

--- scene_main/Controller.java
+++ scene_main/Controller.java
@@ -7,7 +7,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 
-public class scene_main_Controller {
+public class Controller {
 
     @FXML
     private ResourceBundle resources;
