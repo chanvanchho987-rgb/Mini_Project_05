3b74df745344255800b173b3ea00227e915eae45
sva <chanvanchho987@gmail.com>
2026-06-22 16:02:15 +0700

    u

--- scene_main/scene_main_Controller.java
+++ scene_main/scene_main_Controller.java
@@ -1,5 +1,6 @@
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
@@ -19,8 +20,14 @@ public class scene_main_Controller {
     private ListView<?> listview;
 
     @FXML
+    void on_create(ActionEvent event) {
+
+    }
+
+    @FXML
     void initialize() {
-        
+        assert label_status != null : "fx:id=\"label_status\" was not injected: check your FXML file 'View.fxml'.";
+        assert listview != null : "fx:id=\"listview\" was not injected: check your FXML file 'View.fxml'.";
 
     }
 
