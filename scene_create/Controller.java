7c170bd2ecd9413f80f05c9276ae2ab70c0d3fbd
sva <chanvanchho987@gmail.com>
2026-06-23 14:48:08 +0700

    u

---
+++ scene_create/Controller.java
@@ -0,0 +1,29 @@
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class Controller {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private TextField textfield;
+
+    @FXML
+    void on_save(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+       
+
+    }
+
+}
---
+++ scene_delete/Controller.java
@@ -0,0 +1,29 @@
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class Controller {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    void on_no(ActionEvent event) {
+
+    }
+
+    @FXML
+    void on_yes(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+
+    }
+
+}
